e7ba62a2e4733bbcf48dcc8238bdbe956e3b9fd0
vera <vero971410@gmail.com>
2022-05-27 10:06:38 +0300

    SearchSystem

--- untitled/src/practicum/sprint_4/final_task/HashTableSolution.java
+++ untitled/src/practicum/sprint_4/final_task/HashTableSolution.java
@@ -3,6 +3,47 @@ package practicum.sprint_4.final_task;
 import java.io.*;
 import java.util.Objects;
 
+/*
+* https://contest.yandex.ru/contest/24414/run-report/68629112/
+*
+ * -- ПРИНЦИП РАБОТЫ --
+ *
+ * Задача: реализовать хэш таблицу и разрешать коллизии с помощью метода цепочек или метода открытой адресации.
+ * В данном решении реализовано разрешение коллизий с помощью метода открытой адресации.
+ *
+ * Реализация put:
+ * Я выбрала стратегию линейного пробирования.
+ * Стратегия пробирования — это способ выбрать следующую корзину для выполнения операции (вставки или поиска ключа) и сделать шаг в неё,
+ * если текущая корзина занята неподходящим объектом (не тем, который мы ищем, при поиске и любым — при вставке).
+ * Стратегия линейного пробирования выражается формулой:
+ *  h(k, i) = h(k) + ih(k,i)=h(k)+i
+ *  Читать эту формулу следует так: индекс корзины равен сумме хеш-функции от ключа h(k) и номера шага пробирования i.
+ *
+ * Реализация get:
+ * Вычисляем индекс корзины, используя хеш-функцию: x = h(k)%mod
+ * Если искомый элемент — в этой корзине, то поиск успешно окончен. Если корзина пустая — поиск останавливается.
+ * Иначе продолжаем искать в следующих по порядку корзинах
+ *
+ * Реализация delete:
+ * Если мы не нашли значение которое нужно удалить выводим NONE, если нашли - выводим значение и помечаем как DELETED.
+ * Нельзя просто удалить значение, потому что поиск элемента ведется ровно до тех пор пока мы не встретим пустую в корзину.
+ * Поэтому удаленный индекс должен быть помечен как удаленный, а не как пустой.
+ *
+ * -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+ * Для того чтобы операции в хэш таблице были эффективны должна быть хорошая лавинность. Много DELETED элементов замедляют функцию, и при
+ * определнном количнестве нужно будет делать рехеширование для того чтобы сохранять эффективность, но это долгая операция, которую
+ * в задаче сказано не реализовывать.
+ * В данном случае стратегия линейного пробирования реализует достаточную для входных данных лавинность, так как операции выполняются за O(1).
+ *
+ * -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+ * В среднем все операции выполняются за O(1), за исключением случаев когда нам нужно разрешать коллизии. Рехеширование в задаче реализовано не было,
+ * поэтому пересчет списка не происходит.
+ *
+ *-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+ * Память затрачиваемая на реализацию хэш таблицы составляет O(n), поскольку мы выделяем память только под размер элементов, и не более.
+ *
+* */
+
 public class HashTableSolution {
     private static final String GET = "get";
     private static final String PUT = "put";
@@ -12,20 +53,22 @@ public class HashTableSolution {
         try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
             final int commandsCount = readInt(reader);
-            final HashTable hashTable = new HashTable(commandsCount, writer);
+            final HashTable hashTable = new HashTable(commandsCount);
             for (int i = 0; i < commandsCount; i++) {
                 final String[] line = reader.readLine().split(" ");
                 final String command = line[0];
                 final String key = line[1];
                 if (Objects.equals(command, GET)) {
-                    hashTable.get(key);
+                    String result = hashTable.get(key);
+                    writer.write(result + "\n");
                 }
                 if (Objects.equals(command, PUT)) {
                     final String value = line[2];
                     hashTable.put(key, value);
                 }
                 if (Objects.equals(command, DELETE)) {
-                    hashTable.delete(key);
+                    String deletedItem = hashTable.delete(key);
+                    writer.write(deletedItem + "\n");
                 }
             }
         }
@@ -38,65 +81,69 @@ public class HashTableSolution {
     public static class HashTable {
         private static final String NONE = "None";
         private static final String DELETED = "deleted";
+        private static final int NOT_FOUND_VALUE = -1;
         private static final int PROBING = 11;
         private final Node[] list;
         private final int size;
-        private final BufferedWriter writer;
 
-        HashTable(int size, BufferedWriter writer) {
-            this.writer = writer;
+        HashTable(int size) {
             list = new Node[size];
             this.size = size;
         }
 
         public void put(String key, String value) {
-            int counter = 0;
+            int probingStep = 0;
             final Node nodeToPut = new Node(key, value);
-            int index = getBucketIndex(key, counter);
+            int index = increaseBucketIndex(key, probingStep);
 
             while (list[index] != null) {
                 if (list[index].key.equals(key)) {
                     break;
                 }
-                counter++;
-                index = getBucketIndex(key, counter);
+                probingStep++;
+                index = increaseBucketIndex(key, probingStep);
             }
             list[index] = nodeToPut;
         }
 
-        private int getBucketIndex(String key, int counter) {
-            return (Long.valueOf(key).hashCode() % size + PROBING * counter) % size;
+        public String get(String key) {
+            int index = getIndex(key);
+            return getValueByIndex(index);
         }
 
-        public int get(String key) {
-            int counter = 0;
-            int index = getBucketIndex(key, counter);
+        public int getIndex(String key) {
+            int probingStep = 0;
+            int index = increaseBucketIndex(key, probingStep);
             while (list[index] != null) {
                 Node item = list[index];
                 if (item.key.equals(key)) {
-                    write(item.value);
                     return index;
                 }
-                counter++;
-                index = getBucketIndex(key, counter);
+                probingStep++;
+                index = increaseBucketIndex(key, probingStep);
             }
-            write(NONE);
-            return -1;
+            return NOT_FOUND_VALUE;
         }
 
-        private void write(String value) {
-            try {
-                writer.write(value + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
+        private int increaseBucketIndex(String key, int probingStep) {
+            return (Long.valueOf(key).hashCode() % size + PROBING * probingStep) % size;
+        }
+
+        private String getValueByIndex(int index) {
+            if (index == NOT_FOUND_VALUE){
+                return NONE;
+            } else {
+                return list[index].value;
             }
         }
 
-        public void delete(String key) {
-            int index = get(key);
-            if (index != -1) {
+        public String delete(String key) {
+            int index = getIndex(key);
+            String value = getValueByIndex(index);
+            if (index != NOT_FOUND_VALUE) {
                 list[index].setDeleted();
             }
+            return value;
         }
 
         private static class Node {
