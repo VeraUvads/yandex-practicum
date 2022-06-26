9350351d3b628515d467d9c46776d593b58c55d2
vera <vero971410@gmail.com>
2022-06-26 09:51:37 +0300

    DFS

---
+++ untitled/src/practicum/sprint_6/DFS.java
@@ -0,0 +1,80 @@
+package practicum.sprint_6;
+
+import java.io.*;
+import java.util.*;
+
+public class DFS {
+
+
+    public static void main(String[] args) throws IOException {
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            final String[] line = reader.readLine().split(" ");
+            final int n = Integer.parseInt(line[0]);
+            int m = Integer.parseInt(line[1]);
+            Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+            String[] colors = new String[n + 1];
+            fillMap(map, reader, m, n);
+            int startFrom = Integer.parseInt(reader.readLine());
+            dfsGraph(map, colors, startFrom, writer);
+        }
+    }
+
+    private static void dfsGraph(Map<Integer, ArrayList<Integer>> map, String[] colors, int startFrom, BufferedWriter writer) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(startFrom);
+        while (!stack.isEmpty()) {
+            int vertex = stack.pop();
+            if (colors[vertex] == null) {
+                ArrayList<Integer> list = map.get(vertex);
+                if (list != null) {
+                    for (int i = list.size() - 1; i >= 0; i--) {
+                        if (colors[list.get(i)] == null) {
+                            stack.add(list.get(i));
+                        }
+                    }
+                }
+                writer.write(vertex + " ");
+                colors[vertex] = "grey";
+            }
+        }
+    }
+
+    private static <T> void printList(ArrayList<T> list, Writer writer) {
+        list.forEach(elem -> {
+                    try {
+                        writer.write(String.valueOf(elem));
+                        writer.write(" ");
+                    } catch (IOException e) {
+                    }
+                }
+        );
+    }
+
+    private static void fillMap(Map<Integer, ArrayList<Integer>> map, BufferedReader reader, int m, int n) throws IOException {
+        for (int i = 0; i < m; i++) {
+            final String[] line = reader.readLine().split(" ");
+            final int from = Integer.parseInt(line[0]);
+            final int to = Integer.parseInt(line[1]);
+            if (map.containsKey(from)) {
+                map.get(from).add(to);
+            } else {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(to);
+                map.put(from, list);
+            }
+            if (map.containsKey(to)) {
+                map.get(to).add(from);
+            } else {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(from);
+                map.put(to, list);
+            }
+        }
+        for (int i = 0; i <= n; i++) {
+            if (map.containsKey(i)) {
+                Collections.sort(map.get(i));
+            }
+        }
+    }
+}
