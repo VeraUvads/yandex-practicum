687affe2a192cd9df169f3201a3683ab73dc71fa
vera <vero971410@gmail.com>
2022-09-06 21:08:51 +0300

    Journey

--- untitled/src/practicum/sprint_7/Journey.java
+++ untitled/src/practicum/sprint_7/Journey.java
@@ -1,68 +1,73 @@
 package practicum.sprint_7;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Journey {
 
-    private static int[][] matrix;
-    private static int[] list;
-    private static int maxLine = 0;
-    private static int maxResult = 0;
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            list = getInputArray(reader);
-            matrix = new int[list.length + 1][list.length + 1];
-            for (int i = 1; i <= list.length; i++) {
-                int maxVal = 0;
-                for (int j = 1; j <= list.length; j++) {
-//                    int previousMaxSubCount = matrix[i][j - 1];
-//                    int previous = list[lastIncrease];
-//                    int current = list[j];
-//                    if (previous < current) {
-//                        matrix[i][j] = previousMaxSubCount + 1;
-//                        lastIncrease = j;
-//                    } else {
-//                        matrix[i][j] = previousMaxSubCount;
-//                    }
-                    matrix[i][j] = matrix[i - 1][j]; // почему
-                    if (list[i - 1] == list[j - 1]) {
-                        matrix[i][j] = Math.max(matrix[i][j], maxVal + 1);
-                    }
-                    if (list[j - 1] < list[i - 1]) {
-                        maxVal = Math.max(maxVal, matrix[i - 1][j]);
-                    }
-                }
-                int resultForLine = matrix[i][matrix[i].length - 1];
-                if (resultForLine > maxResult) {
-                    maxResult = resultForLine;
-                    maxLine = i;
-                }
+            int[] first = getInputArray(reader);
+            int[] second = Arrays.stream(first).distinct().sorted().toArray();
+            int[][] matrix = new int[first.length + 1][second.length + 1];
+            fillMatrix(matrix, first, second);
+            System.out.println(matrix[first.length][second.length]);
+            writeResult(first, second, matrix, writer);
+        }
+    }
+
+    private static void writeResult(int[] first, int[] second, int[][] matrix, BufferedWriter writer) throws IOException {
+        int firstIndex = first.length;
+        int secondIndex = second.length;
+        ArrayList<Integer> firstLineIndexes = new ArrayList<>();
+        while (firstIndex != 0 && secondIndex != 0) {
+            if (first[firstIndex - 1] == second[secondIndex - 1]) {
+                firstLineIndexes.add(firstIndex);
+                firstIndex = firstIndex - 1;
+                secondIndex = secondIndex - 1;
+            } else if (matrix[firstIndex][secondIndex] == matrix[firstIndex][secondIndex - 1]) {
+                secondIndex = secondIndex - 1;
+            } else {
+                firstIndex = firstIndex - 1;
             }
-            writer.write(maxResult + "\n");
-//            int previous = Integer.MIN_VALUE;
-//            for (int i = maxLine; i < list.length; i++) {
-//                if (previous < list[i]) {
-//                    writer.write(i + 1 + " ");
-//                    previous = list[i];
-//                }
-//            }
-            for (int i = 1; i <= list.length; i++) {
-                if (matrix[i][i] > matrix[i-1][i-1]) {
-                    writer.write(i+ " ");
+        }
+        printReversedList(firstLineIndexes, writer);
+        writer.write("\n");
+    }
+
+
+    private static <T> void printReversedList(ArrayList<Integer> list, Writer writer) throws IOException {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            writer.write(list.get(i) + " ");
+        }
+    }
+
+    private static void fillMatrix(int[][] matrix, int[] first, int[] second) {
+        for (int i = 1; i <= first.length; i++) {
+            for (int j = 1; j <= second.length; j++) {
+                int firstLetter = first[i - 1];
+                int secondLetter = second[j - 1];
+                if (Objects.equals(firstLetter, secondLetter)) {
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                } else { // todo
+                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
                 }
             }
         }
     }
 
-    private static int[] getInputArray(final BufferedReader reader) throws IOException {
-        final int n = Integer.parseInt(reader.readLine());
-        final StringTokenizer splitter = new StringTokenizer(reader.readLine(), " ");
-        final int[] list = new int[n];
 
-        for (int i = 0; i < n; i++) {
+    private static int[] getInputArray(final BufferedReader reader) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        final StringTokenizer splitter = new StringTokenizer(reader.readLine(), " ");
+        final int[] list = new int[count];
+
+        for (int i = 0; i < count; i++) {
             list[i] = Integer.parseInt(splitter.nextToken());
         }
         return list;
