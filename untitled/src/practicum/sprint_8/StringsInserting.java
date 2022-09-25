5a39b6b5305dce1dbcece7b2c9cc7924698acbd5
vera <vero971410@gmail.com>
2022-09-25 16:43:59 +0300

    StringsInserting

--- untitled/src/practicum/sprint_8/StringsInserting.java
+++ untitled/src/practicum/sprint_8/StringsInserting.java
@@ -1,37 +1,59 @@
 package practicum.sprint_8;
 
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
-public class StringsInserting { // чуть-чуть не проходит по времени
+public class StringsInserting {
 
     public static void main(String[] args) throws IOException {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            String resultString = buildString(bufferedReader);
-            writer.write(resultString);
+            String defaultLine = reader.readLine();
+            ArrayList<Node> array = getNodeArray(reader);
+            int start = 0;
+            for (Node pair : array) {
+                int index = pair.getFirst();
+                String line = pair.getSecond();
+                writer.write(defaultLine.substring(start, index));
+                writer.write(line);
+                start = index;
+            }
+            writer.write(defaultLine.substring(start));
         }
     }
 
-
-    private static String buildString(final BufferedReader reader) throws IOException {
-        String defaultLine = reader.readLine();
-        final StringBuilder builder = new StringBuilder(defaultLine);
+    private static ArrayList<Node> getNodeArray(BufferedReader reader) throws IOException {
         final int n = Integer.parseInt(reader.readLine());
-        final int[] indexBuffer = new int[defaultLine.length() + 1];
-        for (int i = 1; i < indexBuffer.length; i++) {
-            indexBuffer[i] = i;
-        }
-
+        ArrayList<Node> array = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             final StringTokenizer splitter = new StringTokenizer(reader.readLine(), " ");
             final String line = splitter.nextToken();
             final int index = Integer.parseInt(splitter.nextToken());
-            builder.insert(indexBuffer[index], line);
-            for (int j = index; j <= defaultLine.length(); j++) {
-                indexBuffer[j] += line.length();
-            }
+            array.add(new Node(index, line));
         }
-        return builder.toString();
+        array.sort(Comparator.comparingInt(Node::getFirst));
+        return array;
+    }
+
+
+    private static class Node {
+        public int first;
+        public String second;
+
+        public Node(int first, String second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public int getFirst() {
+            return first;
+        }
+
+        public String getSecond() {
+            return second;
+        }
     }
 }
